d6088e883fb0fd53b36192463eb2af40c5951af4
tomashm01 <tomashidalgomartin@gmail.com>
2021-11-22 19:08:25 +0100

    test carpeta added

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 22 nov. 2021
+ *      Author: tomas
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ test/test.h
@@ -0,0 +1,16 @@
+/*
+ * test.h
+ *
+ *  Created on: 22 nov. 2021
+ *      Author: tomas
+ */
+
+#ifndef TEST_TEST_H_
+#define TEST_TEST_H_
+
+class test {
+public:
+	test();
+};
+
+#endif /* TEST_TEST_H_ */
