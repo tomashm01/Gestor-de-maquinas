afae724c59ecd6462eb8024cd87f9cf9ae1d8be1
JavierGalvez Martinez <i02gamaj@uco.es>
2021-12-13 23:45:30 +0100

    maquinas terminadas

---
+++ Entities/maquina.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <list>
+
+#ifndef _Maquinas_
+#define _Maquinas_
+
+using namespace std;
+
+class Maquina{
+    private:
+    
+    int id_;
+    int nucleos_;
+    char* descrip_;
+    static std::list<Maquina> listaMaquinas;
+    
+
+    public:
+	Maquina(int id_, int nucleos_, char *descrip_);
+
+    int getId();
+	int getNucleos();
+	char* getDescrip();
+    char* toString();
+
+	void setId(int id_);
+	void setNucelos(int nucleos_);
+	void setDescrip(char* descrip_);
+    	
+
+    bool validarMaquina();
+
+    bool addMaquina(Maquina maquina);
+
+    bool deleteMaquina(Maquina maquina);
+
+    void changesMaquinaByID(int id_ , Maquina maquina);
+
+}
+
+#endif
