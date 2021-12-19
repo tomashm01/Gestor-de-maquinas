b2eaf154177120aae44f20e2de25a0c45070a1d3
tomashm01 <tomashidalgomartin@gmail.com>
2021-12-19 14:30:22 +0100

    menu admin usuarios acabado

--- views/view.cpp
+++ views/view.cpp
@@ -23,9 +23,9 @@ bool verificaDNI(string dni){
 int main(void){
 
 	Menu principal=Menu(0,"Menu Principal",{"Iniciar Sesion","Registrarse","Salir"});
-	Menu menuUsuario=Menu(0,"Menu Usuario",{"Reservar","Cancelar reserva","Ver Reservas","Salir"});
+	Menu menuUsuario=Menu(0,"Menu Usuario",{"Reservar","Cancelar reserva","Ver Reservas","Recuperar contraseña","Salir"});
 	Menu menuMaquina=Menu(0,"Menu Maquina",{"Añadir reserva","Cancelar reserva","Ver Reservas","Salir"});
-	Menu menuAdmin=Menu(0,"Menu Administrador",{"Añadir usuario","Eliminar usuario","Ver usuarios","Recuperar contraseña de un usuario","Salir"});
+	Menu menuAdmin=Menu(0,"Menu Administrador",{"Añadir usuario","Eliminar usuario","Ver usuarios","Modificar usuario","Salir"});
 
 	bool salirApp=false,esUsuario=false,esAdminMaquina=false,esAdminUsuarios=false;
 	string nombre,nickname,password,dni,rol,fechain,fechaout,descripcion;
@@ -67,22 +67,24 @@ int main(void){
 					//Comprobar rol
 					if(usuario.getRol()=="usuario"){
 						esUsuario=true;
-					}else if(usuario.getRol()=="adminMaquinas"){
-						esAdminMaquina=true;
-					}else{
+					}else if(usuario.getRol()=="adminUsuarios"){
 						esAdminUsuarios=true;
+					}else{
+						esAdminMaquina=true;
 					}
 					salirApp=true;
 				}
+				cout<<usuario.getRol()<<endl;
 				opcion=0;
 				break;
 			}
 			case 2:{ //Registrarse
 
-				cout<<"Introduce tu nombre: "<<endl;
-				cin>>nombre;
+				cout<<"Introduce tu nombre(sin espacios ni comas): "<<endl;
+				getline(cin,nombre,' ');
 				cout<<"Introduce tu nickname: "<<endl;
 				cin>>nickname;
+				cin.ignore();
 				do{ //Minimo 8 digitos
 					cout<<"Introduce tu password: "<<endl;
 					cin>>password;
@@ -109,86 +111,190 @@ int main(void){
 		}
 	}while(!salirApp);
 
-	do{
-		menuUsuario.mostrar();
-		cin>>opcion;
-		menuUsuario.setOpcion(opcion);
+	if(esUsuario){
+		do{
+			menuUsuario.mostrar();
+			cin>>opcion;
+			menuUsuario.setOpcion(opcion);
 
-		switch(menuUsuario.getOpcion()){
-			case 1:{//Reservar
+			switch(menuUsuario.getOpcion()){
+				case 1:{//Reservar
 
-				do{
-					cout<<"Introduce maximo dias de reserva:(0-15 dias) "<<endl;
-					cin>>maxdias;
-				}while(maxdias<0 || maxdias>15);
+					do{
+						cout<<"Introduce maximo dias de reserva:(0-15 dias) "<<endl;
+						cin>>maxdias;
+					}while(maxdias<0 || maxdias>15);
 
-				//Obtener dia/mes/año actual y sumar maxdias			
-				fechain=to_string(dia)+"/"+to_string(mes)+"/"+to_string(anio);
-				//Valido la fecha out es correctamente
-				diaOut=maxdias+dia;
-				//Valido fecha final
-				if(diaOut>31){
-					diaOut=diaOut-31;
-					mesOut=mes+1;
-					if(mesOut>12){
-						mesOut=1;
-						anioOut=anio+1;
+					//Obtener dia/mes/año actual y sumar maxdias			
+					fechain=to_string(dia)+"/"+to_string(mes)+"/"+to_string(anio);
+					//Valido la fecha out es correctamente
+					diaOut=maxdias+dia;
+					//Valido fecha final
+					if(diaOut>31){
+						diaOut=diaOut-31;
+						mesOut=mes+1;
+						if(mesOut>12){
+							mesOut=1;
+							anioOut=anio+1;
+						}
+					}else{
+						diaOut=dia;
+						mesOut=mes;
+						anioOut=anio;
 					}
-				}else{
-					diaOut=dia;
-					mesOut=mes;
-					anioOut=anio;
+
+					maquinaTemp=Maquina(0,0,"");
+					bool bandera=false;
+					do{
+						maquinaTemp.showMaquinas();
+						cout<<"Introduce un id de maquina: "<<endl;
+						cin>>idMaquina;
+						maquinaTemp=Maquina(idMaquina,0,"");
+						if(!maquinaTemp.validarMaquina()) bandera=true;
+
+					}while(!bandera);
+
+					id=rand()%1000000;
+
+					fechaout=to_string(diaOut)+"/"+to_string(mesOut)+"/"+to_string(anioOut);
+					reservaTemp=Reserva(id,usuario.getDni(),idMaquina,maxdias,fechain,fechaout);
+
+					//Compruebo que la reserva es correcta
+					if(reservaTemp.addReserva()){
+						reserva=reservaTemp;
+						cout<<"Reserva realizada correctamente"<<endl;
+					}else{
+						cout<<"No se ha podido reservar"<<endl;
+					}
+					esUsuario=true;
+					break;
 				}
-
-				maquinaTemp=Maquina(0,0,"");
-				bool bandera=false;
-				do{
-					maquinaTemp.showMaquinas();
-					cout<<"Introduce un id de maquina: "<<endl;
-					cin>>idMaquina;
-					maquinaTemp=Maquina(idMaquina,0,"");
-					if(!maquinaTemp.validarMaquina()) bandera=true;
-
-				}while(!bandera);
-				
-				id=rand()%1000000;
-
-				fechaout=to_string(diaOut)+"/"+to_string(mesOut)+"/"+to_string(anioOut);
-				reservaTemp=Reserva(id,usuario.getDni(),idMaquina,maxdias,fechain,fechaout);
-
-				//Compruebo que la reserva es correcta
-				if(reservaTemp.addReserva()){
-					reserva=reservaTemp;
-					cout<<"Reserva realizada correctamente"<<endl;
-				}else{
-					cout<<"No se ha podido reservar"<<endl;
+				case 2:{ //Cancelar reserva 
+					cout<<"Introduce tu id de reserva: "<<endl;
+					cin>>id;
+					reserva=Reserva(id,usuario.getDni(),0,0,"","");
+					if(reserva.deleteReserva()){
+						cout<<"Reserva cancelada correctamente"<<endl;
+					}else{
+						cout<<"No se ha podido cancelar la reserva"<<endl;
+					}
+					esUsuario=true;
+					break;
 				}
-				esUsuario=true;
-				break;
-			}
-			case 2:{ //Cancelar reserva 
-				cout<<"Introduce tu id de reserva: "<<endl;
-				cin>>id;
-				reserva=Reserva(id,usuario.getDni(),0,0,"","");
-				if(reserva.deleteReserva()){
-					cout<<"Reserva cancelada correctamente"<<endl;
-				}else{
-					cout<<"No se ha podido cancelar la reserva"<<endl;
+				case 3:{ //Ver reservas
+					cout<<"Tus reservas: "<<endl;
+					reserva.showReservas(usuario.getDni());
+					esUsuario=true;
+					break;
 				}
-				esUsuario=true;
-				break;
+				case 4:{//Cambiar contraseña
+					do{
+						cout<<"Introduce tu nueva contraseña: "<<endl;
+						cin>>password;	
+					}while(password.length()<8);
+					usuario.setPassword(password);
+					if(usuario.changeUser()){
+						cout<<"Contraseña cambiada correctamente"<<endl;
+					}else{
+						cout<<"No se ha podido cambiar la contraseña"<<endl;
+					}
+				}
+				case 5: {//Cerrar sesion
+					return 0;
+				}
 			}
-			case 3:{ //Ver reservas
-				cout<<"Tus reservas: "<<endl;
-				reserva.showReservas(usuario.getDni());
-				esUsuario=true;
-				break;
-			}
-			case 4: //Cerrar sesion
-				return 0;
-		}
-	}while(esUsuario);
+		}while(esUsuario);
+	}
 
+	else if(esAdminUsuarios){
+		do{
+			menuAdmin.mostrar();
+			cin>>opcion;
+			menuAdmin.setOpcion(opcion);
+			switch(menuAdmin.getOpcion()){
+				case 1:{//Añadir usuario
+
+					cout<<"Introduce el nombre(sin espacios ni comas): "<<endl;
+					cin>>nombre;
+
+					cout<<"Introduce el nickname: "<<endl;
+					cin>>nickname;
+
+					do{
+						cout<<"Introduce la password: "<<endl;
+						cin>>password;
+					}while(password.length()<8);
+					
+					do{
+						cout<<"Introduce el dni: "<<endl;
+						cin>>dni;
+					}while(!verificaDNI(dni));
+
+					do{
+						cout<<"Introduce su rol: "<<endl;
+						cin>>rol;
+					}while(rol!="usuario" && rol!="adminUsuarios" && rol!="adminMaquinas");
+
+					usuario=Usuario(dni,password,nickname,nombre,rol);
+
+					if(usuario.addUsuario()){
+						cout<<"Usuario registrado correctamente"<<endl;
+						salirApp=true;
+					}else{
+						cout<<"Error al registrar usuario"<<endl;
+					}
+					break;
+				}
+				case 2:{ //Eliminar usuario
+					usuario.showAllUsers();
+					cout<<"Introduce el dni a eliminar: "<<endl;
+					cin>>dni;
+					usuario=Usuario(dni,"","","","");
+					if(usuario.deleteUsuario()){
+						cout<<"Usuario eliminado correctamente"<<endl;
+					}else{
+						cout<<"No se ha podido eliminar el usuario"<<endl;
+					}
+					break;
+				}
+				case 3:{//Ver usuarios
+					usuario.showAllUsers();
+					break;
+				}
+				case 4:{ //Cambiar datos usuario
+					usuario.showAllUsers();
+					cout<<"Introduce el dni del usuario a modificar: "<<endl;
+					cin>>dni;
+
+					usuarioTemp=Usuario(dni,"","","","");
+
+					cout<<"Introduce el nuevo nombre: "<<endl;
+					cin>>nombre;
+					cout<<"Introduce el nuevo nickname: "<<endl;
+					cin>>nickname;
+
+					cout<<"Introduce la nueva password: "<<endl;
+					cin>>password;
+
+					usuarioTemp.setNombre(nombre);
+					usuarioTemp.setNickname(nickname);
+					usuarioTemp.setPassword(password);
+
+					if(usuarioTemp.changeUser()){
+						cout<<"Usuario modificado correctamente"<<endl;
+					}else{
+						cout<<"No se ha podido modificar el usuario"<<endl;
+					}
+					break;
+				}
+				case 5:{//Salir
+					return 0;
+				}
+			}
+		}while(esAdminUsuarios);
+	}else{
+
+	}
 	
 	return 0;
 
